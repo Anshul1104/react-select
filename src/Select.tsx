50b488a47148fe9f1f8735250b2524baf21b8c65
Anshul Gupta <anshul.gupta36@gmail.com>
2024-05-07 03:44:46 +0530

    update select.tsx

--- src/Select.tsx
+++ src/Select.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import styles from './select.module.css'
 
 export type SelectOption = {
